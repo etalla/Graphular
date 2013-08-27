5e68da76ae528ef580790ee872fafda9c27d7ced
Alexandros Marinos <alexandros@rulemotion.com>
2013-08-27 15:05:12 +0100

    make animation work

--- graph-menu.js
+++ graph-menu.js
@@ -3,7 +3,16 @@ app = angular.module("graphmenu", ["ngAnimate"]);
 function MenuCtrl($scope, $http) {
     $http.get("menu.json").then(function (response) {
         $scope.option = {
-            options: response.data
+            options: response.data,
+            root: true
         };
+
+        $scope.ancestorsSelected = function(focus){
+            while(1){
+                focus = focus.$parent;
+                if (focus.option.root) return true;
+                if (focus.$parent.choice != focus.option.title) return false;
+            }
+        }
     });
 }
